541240b8f682afeb300b345f34bb9308244f561a
davi <davijluna@gmail.com>
2023-03-30 10:42:12 -0300

    refactor: adicionando comentarios de explicação

--- aula50.js
+++ aula50.js
@@ -1,5 +1,7 @@
 // Nessa parte aprende sobre o arguments que guarda os valores da funcao quando ela não tem os parametros
 
+// Mas detalhe esse arguments só funciona com funções com nome function com function com arrowFunction não funciona.
+
 function funcao() {
   let total = 0;
   for (let argumento of arguments) {
