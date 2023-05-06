214b930d59d001c537d293ebf1cd495403ba4738
davi <davijluna@gmail.com>
2023-05-05 23:47:06 -0300

    feat: adicionando alteração de parametros para as funções

--- aula52.js
+++ aula52.js
@@ -1,12 +1,17 @@
-function retornaFuncao() {
-  const nome = 'Davi';
+function retornaFuncao(nome) {
+  // const nome = 'Davi';
   return function() {
     return nome
   }
 }
 
 //  exemplo de closures
+// adicionando alteração de parametros para as funções
 
-const funcao = retornaFuncao();
+const funcao = retornaFuncao('Davi');
+const funcao2 = retornaFuncao('joão');
 
 console.log(funcao)
+
+console.log(funcao, funcao2)
+
