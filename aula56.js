1d3bfe9f7ced9664717256bb1d9bea9e24942c0f
davi <davijluna@gmail.com>
2023-05-09 23:20:50 -0300

    feat: usando get set e this para um objeto ser acessado

--- aula56.js
+++ aula56.js
@@ -4,12 +4,25 @@ function criaPessoa(nome, sobrenome, altura, peso) {
   return {
     nome,
     sobrenome,
-    fala: function(assunto) {
+    fala: function(assunto= 'falando sobre NADA !!!') {
       return `${this.nome} está ${assunto}.`;
     },
     altura: altura,
     peso: peso,
+    // Getter:
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`
+    },
+    // nessa parte usamos o set para adicionar imformaçôes a função nomeCompleto.
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(' ');
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(' ');
+      console.log(valor)
+    },
     // na linha 12 usamos a palavra "get" para fazer o imc "fingir" que é um atributo do objeto.
+    // Getter:
     get imc() {
       const indice = this.peso / (this.altura ** 2);
       return indice.toFixed(2);
@@ -18,8 +31,11 @@ function criaPessoa(nome, sobrenome, altura, peso) {
 }
 
 const p1 = criaPessoa('Davi', 'Jesus', 1.70, 80);
-// Agora na linha 24 e 22 nos retiramos os "()" do imc e agora ele fica fingindo ser um atributo do objeto>
+// Agora na linha 24 e 22 nos retiramos os "()" do imc e agora ele fica fingindo ser um atributo do objeto
 console.log(p1.imc)
 const p2 = criaPessoa('Pamela', 'Roustin', 1.60, 42)
-console.log(p2.imc)
-console.log(p2.fala('falando sobre JS'));
+p2.nomeCompleto = 'Giulia Silva Torres';
+// console.log(p2.imc)
+console.log(p2.nomeCompleto)
+console.log(p1.sobrenome);
+console.log(p2.fala());
