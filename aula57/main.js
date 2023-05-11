8f7c8b243fc5d6efea8a3e2abae615f0ea88be38
davi <davijluna@gmail.com>
2023-05-11 00:54:31 -0300

    feat: criando os primeiros comandos da calculadora e fazendo modificações aos motões

--- aula57/main.js
+++ aula57/main.js
@@ -1,1 +1,31 @@
 // Calculadora com factory function
+
+function criaCalculadora() {
+  // factory function é basicamente uma função que retorna um Objeto
+  return {
+    display: document.querySelector('.display'),
+
+
+
+    inicia() {
+      this.cliqueBotoes();
+    },
+
+    cliqueBotoes(){
+      // this -> calculadora
+      document.addEventListener('click', function(e) {
+         const el = e.target;
+        console.log(this);
+         if(el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText);
+         }
+      }.bind(this));
+    },
+    btnParaDisplay(valor) {
+      this.display.value += valor
+    }
+  };
+}
+
+ const calculadora = criaCalculadora();
+ calculadora.inicia();
