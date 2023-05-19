74bea940f5b227ed38172e6008212db19f1bf940
davi <davijluna@gmail.com>
2023-05-18 23:32:16 -0300

    feat: adicionando rascunho e aula 50

--- RASCUNHO.js
+++ RASCUNHO.js
@@ -1,15 +1,16 @@
-let number = 10
+function pessoa(nome, idade) {
+  return {
+    nome: idade,
+    idade: nome,
+    
 
-if(number % 3 == 0 && number % 5 == 0) {
-  console.log('fizzBuzz')
-  return 'fizzBuzz'
+
+  }
 }
-if(number % 5 == 0) {
-  console.log('fizz')
-  return 'fizz'
-}
-if(number % 3 == 0) {
-  console.log('buzz')
-  return 'buzz'
-}
+
+const informacão = pessoa('Davi', 35);
+
+console.log(informacão.idade)
+
+
 
--- aula50.js
+++ aula50.js
@@ -1,6 +1,6 @@
 // Nessa parte aprende sobre o arguments que guarda os valores da funcao quando ela não tem os parametros
 
-// Mas detalhe esse arguments só funciona com funções com nome function com function com arrowFunction não funciona.
+// Mas detalhe esse arguments só funciona, com funções com nome function com arrowFunction não funciona.
 
 function funcao(a, b = 2) {
   console.log(a + b);
