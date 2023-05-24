020f1c0b8071cc696b3e32865b299926401fb728
davi <davijluna@gmail.com>
2023-05-23 23:37:10 -0300

    refactor: addicionando a função calculadora e enicioando a intrudução das arrow function no curso

---
+++ aula59/main.js
@@ -0,0 +1,20 @@
+function Calculadora() {
+  this.display = document.querySelector('.display');
+
+  this.capturaCliques = () => {
+    document.addEventListener('click', event => {
+      const el = event.target;
+      if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+    });
+  };
+
+  this.addNumDisplay = el => this.display.value += el.innerText;
+
+  this.inicia = () => {
+    this.capturaCliques();
+  };
+
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
