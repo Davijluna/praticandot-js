d137f441ad009c2aaa160d09dbb1d5f8552ba25a
davi <davijluna@gmail.com>
2023-05-24 14:52:50 -0300

    feat: criando a função para limpar o display e colocando para arrow function

--- aula59/main.js
+++ aula59/main.js
@@ -6,11 +6,19 @@ function Calculadora() {
   this.capturaCliques = () => {
     document.addEventListener('click', event => {
       const el = event.target;
-      if (el.classLiet.contains('btn-num')) this.addNumDisplay(el);
+      if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+      // adicionando as funções do clear, delete e do adiciona conta.
+      if (el.classList.contains('btn-clear')) this.clear();
+      if (el.classList.contains('btn-del')) this.del(el);
+      if (el.classList.contains('btn-eq')) this.realizaConta(el);
     });
   };
  // METODO.
   this.addNumDisplay = el => this.display.value += el.innerText;
+// ESTA PARTE LIMPA O DISPLAY DA CALCULADORA 
+  this.clear = () => {
+    this.display.value = '';
+  }
  // METODO.
   this.inicia = () => {
      // AQUI ESTA CHAMANDO O METODO >> capturaCliques
