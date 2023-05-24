2418b6b85999def9860f92f385ac4c8a2657912f
davi <davijluna@gmail.com>
2023-05-23 23:42:21 -0300

    comentario: adicionando comentario para melhor compreenção

--- aula59/main.js
+++ aula59/main.js
@@ -1,16 +1,19 @@
-function Calculadora() {
+function Calculadora() { 
+  // Criando os metodos da calculadora
+  // METODO.
   this.display = document.querySelector('.display');
-
+ // METODO.
   this.capturaCliques = () => {
     document.addEventListener('click', event => {
       const el = event.target;
       if (el.classLiet.contains('btn-num')) this.addNumDisplay(el);
     });
   };
-
+ // METODO.
   this.addNumDisplay = el => this.display.value += el.innerText;
-
+ // METODO.
   this.inicia = () => {
+     // AQUI ESTA CHAMANDO O METODO >> capturaCliques
     this.capturaCliques();
   };
 
