ff22646e3d989e98308756d782ad1d70879749f0
davi <davijluna@gmail.com>
2023-05-26 14:28:51 -0300

    feat: exercicio com copia de array que não interfere nas demais

--- aula62.js
+++ aula62.js
@@ -18,8 +18,12 @@ const novo = nomes;
 delete novo[1];
 
 nomes[1] = 'Maria'
+
+// Agora não altera o array novo nem nomes o novo2 é independente.
+const novo2 = [...nomes]
+novo2[3] = 'Davi';
 // o que for feito em novo reflete em nomes.
-console.log(novo, nomes)
+console.log(novo, nomes, novo2)
 
 
 
