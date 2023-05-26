12e01eb99755dd3053ee07289c6ea62cd862ccc9
davi <davijluna@gmail.com>
2023-05-26 14:23:51 -0300

    feat: exercico de copia de array que reflete de um array para o outro

--- aula62.js
+++ aula62.js
@@ -1,12 +1,25 @@
+
+// nomes[2] = 'Davi';
+
+// delete nomes[1]
+
+// nomes[1] = 1
+
+// const nomes2 = new Array('Davi', 'Pamela', 'Adriana')
+
+// console.log(nomes)
+// console.log(nomes2)
+
+// FAZENDO COPIA DA ARRAY.
+
 const nomes= ['Eduardo', 'Maria', 'Joana'];
 
-nomes[2] = 'Davi';
+const novo = nomes;
+delete novo[1];
 
-delete nomes[1]
+nomes[1] = 'Maria'
+// o que for feito em novo reflete em nomes.
+console.log(novo, nomes)
 
-nomes[1] = 1
 
-const nomes2 = new Array('Davi', 'Pamela', 'Adriana')
 
-console.log(nomes)
-console.log(nomes2)
