8d923829111ee0256b41992be2ddc86a72cebfb4
davi <davijluna@gmail.com>
2023-05-26 00:25:51 -0300

    feat: exercicios com função geradora

--- aula61.js
+++ aula61.js
@@ -1,1 +1,40 @@
 // FUNÇÕES GERADORAS
+
+function* geradora1() {
+  yield 'valor 1';
+
+  yield 'valor 2';
+
+  yield 'valor 3';
+}
+
+const g1 = geradora1();
+
+for (let valor of g1) {
+  console.log(valor);
+}
+
+// console.log(g1.next().value)
+// console.log(g1.next().value)
+// console.log(g1.next())
+// console.log(g1.next())
+
+
+function* gerador2() {
+  let i = 0;
+
+  while(true) {
+    yield i;
+     i++;
+  }
+}
+
+const g2 = gerador2();
+  console.log(g2.next().value)
+  console.log(g2.next().value)
+  console.log(g2.next().value)
+  console.log(g2.next().value)
+  console.log(g2.next().value)
+  console.log(g2.next().value)
+  console.log(g2.next().value)
+  console.log(g2.next().value)
