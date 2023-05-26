9f930182d49d8580d51a0669de750602576ec115
davi <davijluna@gmail.com>
2023-05-26 00:50:05 -0300

    feat: adicionando arquivo de revisão de array

---
+++ aula62.js
@@ -0,0 +1,12 @@
+const nomes= ['Eduardo', 'Maria', 'Joana'];
+
+nomes[2] = 'Davi';
+
+delete nomes[1]
+
+nomes[1] = 1
+
+const nomes2 = new Array('Davi', 'Pamela', 'Adriana')
+
+console.log(nomes)
+console.log(nomes2)
