5915ec920e23cd4e3ff1ec969b21a3d9cca049fd
davi <davijluna@gmail.com>
2023-05-30 20:21:58 -0300

    comentario para estudo

--- aula63.js
+++ aula63.js
@@ -7,5 +7,5 @@ const nomes = ['Davi', 'Maria', 'Pedro', 'José'];
 
 // const removidos = nomes.splice(2, 2);
 // utilizando numero negativo
-const removidos = nomes.splice(-2, 1);
+const removidos = nomes.splice(-2, Number.MAX_VALUE); // o MAX_VALUE mostra quantos elementos eu quero remover começando do indice que eu passei.
 console.log(nomes, removidos);
