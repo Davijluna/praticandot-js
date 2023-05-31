705e8ac1bffa4e5787a70b9a3ae9c42531dc25b4
davi <davijluna@gmail.com>
2023-05-31 15:42:34 -0300

    feat: trabalhando com a função filter

--- aula65.js
+++ aula65.js
@@ -1,1 +1,8 @@
 // Filter - Filtrando o array.
+// filter, map, reduce
+
+const numeros = [5, 50, 1, 4, 5, 90, 50, 30, 6, 8, 10];
+
+const retorna = numeros.filter(num => num >= 10)
+
+console.log(retorna);
