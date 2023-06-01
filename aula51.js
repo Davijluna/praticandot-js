079d762a0afb647c0c85d3e21ee12374a993ec0a
davi <davijluna@gmail.com>
2023-06-01 17:03:21 -0300

    refactor: movendo arquivos para pasta de array

--- aula51.js
+++
@@ -1,6 +0,0 @@
-// Retorno de funções.
-// rever aula 
-let frase = 'Oi tudo bem?';
-let separador = frase.split(' ');
-
-console.log(separador);
--- aula59/script.js
+++
@@ -1,3 +0,0 @@
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-
-console.log(animals.slice(0, 4));
