efb1579a78289781acca556c714663ee7acd9a78
davi <davijluna@gmail.com>
2023-06-02 00:19:33 -0300

    feat: utilizando notação de chaves em objetos

--- JavaScript-Objetos-e-prototypes-Avançado/aula70.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula70.js
@@ -1,1 +1,13 @@
 // Revisando Ojetos
+
+const pessoa = {
+  nome: 'Luiz',
+  sobrenome: 'Otávio',
+}
+
+
+// outra maneira de utilizar objetos
+const chave = 'nome';
+
+console.log(pessoa[chave])
+// console.log(pessoa['nome']);s
