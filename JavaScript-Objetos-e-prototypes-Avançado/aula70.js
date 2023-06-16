d897931f29b9e5aca5bada34903c8a652cec5d37
davi <davijluna@gmail.com>
2023-06-16 19:03:09 -0300

    feat: adicionando função para ver idades que serve para ser colocada em formularios

--- JavaScript-Objetos-e-prototypes-Avançado/aula70.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula70.js
@@ -1,44 +1,41 @@
 // Revisando Ojetos
 
 // Metodos são funcões que estão dentro do objeto que executão ações.
-
-
-
-
-
-
-
 // utilizando construtor
+// 
 
 const pessoa1 = new Object();
 pessoa1.nome = 'Luiz';
 pessoa1.sobrenome = 'Otávio';
-pessoa1.idade = 30
-
+pessoa1.idade = 2;
 pessoa1.falarNomre = function() {
-  console.log(`${this.sobrenome} está falando seu nome e tem ${this.idade} anos de idade`);
+  return (`${this.sobrenome} está falando seu nome e tem ${this.idade} anos de idade`);
 }
 
-pessoa1.falarNomre();
-
-delete pessoa1.nome;
-
-console.log(pessoa1)
-
-for (let chave in pessoa1) {
-  console.log(chave, 'aqui pega a chave')
+pessoa1.getDataNascimento = function() {
+  const dataAtual = new Date();
+  return dataAtual.getFullYear() - this.idade; 
 }
 
-for (let chave in pessoa1) {
-  console.log(pessoa1, 'aqui pega o objeto completo')
-}
+console.log(pessoa1.getDataNascimento());
 
-for (let chave in pessoa1) {
-  console.log(pessoa1[chave], 'aqui pega o objeto completo')
-}
+// pessoa1.falarNomre();
 
+// delete pessoa1.nome;
 
+// console.log(pessoa1)
 
+// for (let chave in pessoa1) {
+//   console.log(chave, 'aqui pega a chave')
+// }
+
+// for (let chave in pessoa1) {
+//   console.log(pessoa1, 'aqui pega o objeto completo')
+// }
+
+// for (let chave in pessoa1) {
+//   console.log(pessoa1[chave], 'aqui pega o objeto completo')
+// }
 
 // const pessoa1 = new Object();
 // pessoa1.nome = 'Luiz';
