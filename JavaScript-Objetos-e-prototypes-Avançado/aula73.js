09a5f5a3b67d167f14775a2b5e6c64808702b501
davi <davijluna@gmail.com>
2023-06-23 16:44:18 -0300

    feat: utilizando o operador ... para espalhar o objeto

--- JavaScript-Objetos-e-prototypes-Avançado/aula73.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula73.js
@@ -1,1 +1,10 @@
 // Métodos úteis para objetos
+// utilizando spred operation
+const produto = {nome: 'Caneca', preco: 1.8};
+const novoValor = { ...produto };
+
+novoValor.nome = 'Davi';
+novoValor.preco = 'Muito caro !!!!!';
+
+console.log(produto)
+console.log(novoValor)
