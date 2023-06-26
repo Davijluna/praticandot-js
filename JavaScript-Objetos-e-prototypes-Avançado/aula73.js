11ad224247c20b22e3b94d46421f37d54c8dbaa6
davi <davijluna@gmail.com>
2023-06-26 00:56:18 -0300

    feat: utilizando Object.values para obter os valores de um objeto

--- JavaScript-Objetos-e-prototypes-Avançado/aula73.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula73.js
@@ -5,18 +5,23 @@
 // utilizando Object.key
 // utilizando Object.freeze
 // Object.getOwnPropertyDescriptor(o, 'prop')
-
 //Object.getOwnPropertyDescriptor >> retorna o que essa propiedade é 
+
+
+// utilizando Object.values
+// com esse Object.value nos obtemos os valores ao invés das chaves que era quando tinhamos os no Object.keys 
+
 const produto = { nome: 'Caneca', preco: 1.8 };
-Object.defineProperty(produto, 'nome', {
-  writable:false,
-  configurable:false,
-})
+// console.log(Object.keys(produto))
+console.log(Object.values(produto))
+
+
+// Object.defineProperty(produto, 'nome', {
+//   writable:false,
+//   configurable:false,
+// })
 // console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
-produto.nome= 'Outra coisa !!!'
-console.log(produto)
-
-
+// produto.nome= 'Outra coisa !!!'
 
 // Object.freeze(produto);
 // produto.nome = 'Outro nome';
