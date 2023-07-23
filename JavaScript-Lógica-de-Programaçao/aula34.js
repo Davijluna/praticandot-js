a31c6b5855ea1edeccc977f0882e22d293b85ea7
davi <davijluna@gmail.com>
2023-07-22 21:46:57 -0300

    feat: utilizando for e length para percorrer indece de array

--- JavaScript-Lógica-de-Programaçao/aula34.js
+++ JavaScript-Lógica-de-Programaçao/aula34.js
@@ -1,10 +1,19 @@
 // For-Clássico - Estrutura de Repetição.
 
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+
+for(let i = 0; i < frutas.length; i+= 1) {
+  console.log(frutas[i]);
+}
+
+
+// TRABALHANDO COM FOR CLÁSSICO 
 // for(let i = 0; i <= 5; i += 1) {
 //   console.log(i);
 // 
 
-for(let i = 0; i <= 5; i += 1) {
-  const par = i % 2 == 0 ? 'Par' : 'Impar'
-  console.log(par)
-}
+// UTILIZANDO TERNÁRIO NA ESTRUTURA DE REPETIÇÃO
+// for(let i = 0; i <= 5; i += 1) {
+//   const par = i % 2 == 0 ? 'Par' : 'Impar'
+//   console.log(par)
+// }
