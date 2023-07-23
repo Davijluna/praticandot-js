7c60d64ece81357ef79e91f23dfae725853fadb6
davi <davijluna@gmail.com>
2023-07-22 23:07:25 -0300

    feat: mostrando os diferentes tipos de for mostrando a sintaxe e explicando o seus funcionamentos

--- JavaScript-Lógica-de-Programaçao/aula37.js
+++ JavaScript-Lógica-de-Programaçao/aula37.js
@@ -1,1 +1,25 @@
 // For of - Estrutura de repetição.
+
+const nome = 'Luiz Otávio';
+//  DEMONSTRANDO OS DIFERENTER TIPODE FOR OBS: FALTA O FOREACH
+
+// For clássico - Geralmente com iteráveis (array ou strings)
+// For in - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (iteráveis, arrays ou strings)
+
+// For clássico.
+// for(let i = 0; i < nome.length; i += 1) {
+//   console.log(nome[i])
+// }
+
+// for in.
+// for(let valor in nome) {
+//   console.log(nome[valor])
+// }
+
+for(let valor of nome) {
+  if( valor === 'O') {
+    console.log('Tem letra O')
+  }
+    console.log('Não têm')
+}
