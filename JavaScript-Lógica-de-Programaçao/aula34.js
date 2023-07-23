598f64e0370f07db0d2ac7bf93e24e791ecc96fa
davi <davijluna@gmail.com>
2023-07-22 21:37:29 -0300

    feat: trabalhando com for clássico e usando ternário para estrutura de repetição

--- JavaScript-Lógica-de-Programaçao/aula34.js
+++ JavaScript-Lógica-de-Programaçao/aula34.js
@@ -1,1 +1,10 @@
 // For-Clássico - Estrutura de Repetição.
+
+// for(let i = 0; i <= 5; i += 1) {
+//   console.log(i);
+// 
+
+for(let i = 0; i <= 5; i += 1) {
+  const par = i % 2 == 0 ? 'Par' : 'Impar'
+  console.log(par)
+}
