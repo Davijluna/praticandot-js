c516b1895bfe006ebe04f50adfa963755969d397
davi <davijluna@gmail.com>
2023-07-24 23:38:40 -0300

    feat:teatativa de solução

--- JavaScript-Objetos-e-prototypes-Avançado/aula77/validadorDeCPF.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula77/validadorDeCPF.js
@@ -1,7 +1,36 @@
 // Criando um validador de CPF.
 
-const CPF = [7, 0, 5, 4, 8, 4, 4, 5, 0, 5, 2]
+const CPF = '705.484.450-52'
+const cpfLimpo = CPF.replace(/\D+/g, '');
 
-for(let i in CPF) {
-  CPF[i]
+let numero = cpfLimpo
+
+let num2 = 11
+let num = []
+let recebe;
+let convert = 0
+
+
+for(let index = 0; index < numero.length; index += 1) {
+
+  num += numero[index] * num2
+  num2 -= 1
+  convert = (Number(num)) + convert
+
+  console.log(convert)
 }
+
+
+
+
+
+
+// const arraycpf = Array.from(cpfLimpo)
+// let coletaNumber = 0;
+
+//   for(const contador of arraycpf) {
+//     coletaNumber += parseInt(contador)
+//   }
+
+// console.log(coletaNumber);
+
