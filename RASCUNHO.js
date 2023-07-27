a1b68cab50a9d4367547345d01a5062dc12a1e25
davi <davijluna@gmail.com>
2023-07-27 00:44:01 -0300

    feat: criando rascunho e adicionando ao arquivo para entender solução feita em aula

--- RASCUNHO.js
+++ RASCUNHO.js
@@ -1,16 +1,43 @@
-function pessoa(nome, idade) {
-  return {
-    nome: idade,
-    idade: nome,
+// function pessoa(nome, idade) {
+//   return {
+//     nome: idade,
+//     idade: nome,
     
 
 
-  }
+//   }
+// }
+
+// const informacão = pessoa('Davi', 35);
+
+// console.log(informacão.idade)
+
+// Aqui eu criei a função com que vai passar as heranças para as demais.
+function Protuto(nome, preco) {
+  this.nome = nome;
+  this.preco = preco;
 }
 
-const informacão = pessoa('Davi', 35);
+// passando para o prototype do Protudo a função que aumenta os valores.
+Protuto.prototype.aumento = function(valor) {
+  this.preco += valor;
+}
 
-console.log(informacão.idade)
+// Criando a função que herdara os atributos da função Produto.
+// Na função alêm do que já é herdado podemos acrescentar atributos também isso aconteceu na cor nessa funçao.
+function Fusca(nome, preco, cor) {
+  // Utilizamos o call que é uma espécie de this (toda função têm um 'call').
+  Protuto.call(this, nome, preco, cor)
+}
 
+// Associando o prototype da função Fusca ao prototype da função Produto usando Object.create().
+Fusca.prototype = Object.create(Protuto.prototype)
 
+// Criando uma constante e inicializando a função Fusca com new e adicionando os valores que já tinha sido herdados 
+const fusca = new Fusca('meu Fusca', 1000, 'preto')
 
+// chamando a função aumento e adicionando um novo valor.
+fusca.aumento(10)
+
+// Chamando fusca no console  para ver todo o returno no terminal.
+console.log(fusca)
