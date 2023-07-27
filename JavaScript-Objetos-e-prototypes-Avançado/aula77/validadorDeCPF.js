504f158fe7ea811b9155a62bc45000a8d49d07a0
davi <davijluna@gmail.com>
2023-07-27 13:33:26 -0300

    feat: validação do cpf versão do Professor

--- JavaScript-Objetos-e-prototypes-Avançado/aula77/validadorDeCPF.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula77/validadorDeCPF.js
@@ -1,70 +1,91 @@
-// Criando um validador de CPF.
-/**
- * Recebe os CPFs para verificar.
- */
-const CPF = '111.111.111-11'
-/**
- * Limpando os caracteres que não são números
- * 
- */
-const cpfLimpo = CPF.replace(/\D+/g, '');
-let numero = cpfLimpo
+// // Criando um validador de CPF.
 
-let num2 = 10
-let num3 = 11
-
-let primeiro = 0
-let segundo = 0
-
-let recebe;
-let convertPrimeiro = 0
-let convertSegundo = 0
-let totalPrimeiro = 0;
-let totalSegundo = 0;
-
-let somaReduce;
-
-for(let index = 0; index < numero.length; index += 1) {
-  if(cpfLimpo != 11 && cpfLimpo.length === '00000000000') {
-    console.log('CPF inválido !!!!')
-  } else {
-  if(num2 >= 2) {
-      primeiro += numero[index] * num2
-      convertPrimeiro = (Number(primeiro))
-      totalPrimeiro += convertPrimeiro
-      primeiro = '';
-      convertPrimeiro = 0
-      num2 -= 1
+// Código do Professor 
+function ValidaCpf(cpfEnviado) {
+  Object.defineProperty(this, 'cpfLimpo', {
+    enumerable: true,
+    get: function() {
+      return cpfEnviado.replace(/\D+/g, '')
     }
-  }
+  })
 }
 
-for(let index = 0; index < numero.length; index += 1) {
-  if(cpfLimpo != 11 && cpfLimpo.length === '00000000000') {
-    console.log('CPF inválido !!!!')
-  } else {
-    if(num3 >= 2) {
-      segundo += numero[index] * num3
-      convertSegundo = (Number(segundo))
-      totalSegundo += convertSegundo
-      segundo = '';
-      convertSegundo = 0
-      num3 -= 1
-    } 
-  }
+ValidaCpf.prototype.Valida = function() {
+  if(typeof this.cpfLimpo === 'undefined') return false;
+  if(this.cpfLimpo.length !== 11) return false
+  return true;
+};
+
+const cpf = new ValidaCpf('705.484.450-52');
+// console.log(cpf.cpfLimpo)
+console.log(cpf.Valida())
+
+// /**
+//  * Recebe os CPFs para verificar.
+//  */
+// const CPF = '705.484.450-52'
+// /**
+//  * Limpando os caracteres que não são números
+//  * 
+//  */
+// const cpfLimpo = CPF.replace(/\D+/g, '');
+// let numero = cpfLimpo
+
+// let num2 = 10
+// let num3 = 11
+
+// let primeiro = 0
+// let segundo = 0
+
+// let recebe;
+// let convertPrimeiro = 0
+// let convertSegundo = 0
+// let totalPrimeiro = 0;
+// let totalSegundo = 0;
+
+// let somaReduce;
+
+// for(let index = 0; index < numero.length; index += 1) {
+//   if(cpfLimpo != 11 && cpfLimpo.length === '00000000000') {
+//     console.log('CPF inválido !!!!')
+//   } else {
+//   if(num2 >= 2) {
+//       primeiro += numero[index] * num2
+//       convertPrimeiro = (Number(primeiro))
+//       totalPrimeiro += convertPrimeiro
+//       primeiro = '';
+//       convertPrimeiro = 0
+//       num2 -= 1
+//     }
+//   }
+// }
+
+// for(let index = 0; index < numero.length; index += 1) {
+//   if(cpfLimpo != 11 && cpfLimpo.length === '00000000000') {
+//     console.log('CPF inválido !!!!')
+//   } else {
+//     if(num3 >= 2) {
+//       segundo += numero[index] * num3
+//       convertSegundo = (Number(segundo))
+//       totalSegundo += convertSegundo
+//       segundo = '';
+//       convertSegundo = 0
+//       num3 -= 1
+//     } 
+//   }
   
-}
-const somaReducePrimeiro = 11 - (totalPrimeiro % 11)
-const somaReduceSegundo = 11 - (totalSegundo % 11)
+// }
+// const somaReducePrimeiro = 11 - (totalPrimeiro % 11)
+// const somaReduceSegundo = 11 - (totalSegundo % 11)
 
-const valorFinal1 = somaReducePrimeiro >= 10 ? 0 : somaReducePrimeiro
-const valorFinal2 = somaReduceSegundo >= 10 ? 0 : somaReduceSegundo
+// const valorFinal1 = somaReducePrimeiro >= 10 ? 0 : somaReducePrimeiro
+// const valorFinal2 = somaReduceSegundo >= 10 ? 0 : somaReduceSegundo
 
-if(somaReduceSegundo === Number(numero.charAt(numero.length - 1)) && somaReducePrimeiro === Number(numero.charAt(numero.length - 2))) {
- console.log('CPF Válido !!!')
-} else {
-  console.log('CPF não Válido')
-}
+// if(somaReduceSegundo === Number(numero.charAt(numero.length - 1)) && somaReducePrimeiro === Number(numero.charAt(numero.length - 2))) {
+//  console.log('CPF Válido !!!')
+// } else {
+//   console.log('CPF não Válido')
+// }
 
 
 // Rascunho para melhor compreensão da solução feita em aula.
@@ -100,7 +121,6 @@ if(somaReduceSegundo === Number(numero.charAt(numero.length - 1)) && somaReduceP
 // console.log(fusca)
 
 
-// Código do Professor 
 
 
 
