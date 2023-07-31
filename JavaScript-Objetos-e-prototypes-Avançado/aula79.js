fdd1dd4a25ce336113fa33f6b8c5322dd6eedee6
davi <davijluna@gmail.com>
2023-07-31 15:53:57 -0300

    feat: utilizando factory function como padrões de projetos e aprendendo o que é mixin

--- JavaScript-Objetos-e-prototypes-Avançado/aula79.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula79.js
@@ -2,27 +2,39 @@
 
 // criando a factory functions e trabalhando com o proto dessa função.
 // criando o prototype na minha própia função.
-function criaPessoa(nome, sobrenome) {
-  const pessoaPrototype = {
-    falar() {
-      console.log(`${this.nome} está falando`)
-    },
+// COMPOSIÇÃO >> 
 
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando`)
+  },
+};
+
+const comer = {
     comer() {
       console.log(`${this.nome} está comer`)
-    },
+  },
+}
 
+const beber = {
     beber() {
       console.log(`${this.nome} está beber`)
-    }
   }
+}
 
-  return {
-    nome,
-    sobrenome
-  };
+// Aqui com Object.assign
+const pessoaPrototype = Object.assign({}, falar, comer, beber);
+
+// Aqui com spreedOperator.
+// const pessoaPrototype = { ...falar, ...comer, ...beber };
+
+function criaPessoa(nome, sobrenome) {
+// linkando o objeto com o prototype.
+  return Object.create(pessoaPrototype, {
+    nome: { value: nome },
+    sobrenome: { value: sobrenome },
+  });
 }
 const p1 = criaPessoa('Davi', 'Jesus de Luna');
-
 
 // console.log(p1.)
