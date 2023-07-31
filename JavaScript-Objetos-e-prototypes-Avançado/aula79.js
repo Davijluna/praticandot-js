7369bec3749250f33352a58434535a96557de371
davi <davijluna@gmail.com>
2023-07-31 14:00:44 -0300

    feat: utilizando o prototype na função factorory

--- JavaScript-Objetos-e-prototypes-Avançado/aula79.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula79.js
@@ -1,1 +1,28 @@
 // Factory Functiosns + Prototypes.
+
+// criando a factory functions e trabalhando com o proto dessa função.
+// criando o prototype na minha própia função.
+function criaPessoa(nome, sobrenome) {
+  const pessoaPrototype = {
+    falar() {
+      console.log(`${this.nome} está falando`)
+    },
+
+    comer() {
+      console.log(`${this.nome} está comer`)
+    },
+
+    beber() {
+      console.log(`${this.nome} está beber`)
+    }
+  }
+
+  return {
+    nome,
+    sobrenome
+  };
+}
+const p1 = criaPessoa('Davi', 'Jesus de Luna');
+
+
+// console.log(p1.)
