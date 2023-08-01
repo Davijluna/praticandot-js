92aa1ab1fbf96505569295365996f4251977cf95
davi <davijluna@gmail.com>
2023-08-01 00:42:54 -0300

    feat: utilizando classe e function contructor

--- JavaScript-Objetos-e-prototypes-Avançado/aula80.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula80.js
@@ -1,1 +1,27 @@
 // Criando classes.
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome =  nome;
+    this.sobrenome = sobrenome;
+  }
+
+  falar() {
+    console.log(`${this.nome} está falando`)
+  }
+}
+
+// CRIANDO A MESMA FUNCIONALIDADE MAS COM FUNCTION CONSTRUCTOR.
+function Pessoa2(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function() {
+  console.log(`${this.nome} está falando.`)
+}
+
+const p1 = new Pessoa('Davi', 'Jesus');
+const p2 = new Pessoa2('Davi', 'Jesus');
+
+console.log(p1.falar())
+console.log(p2.falar())
