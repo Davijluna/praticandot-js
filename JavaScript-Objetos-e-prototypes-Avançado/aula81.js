e2b42d1ebb116b55ce3ef7ebc68997400ff4f425
davi <davijluna@gmail.com>
2023-08-01 18:18:55 -0300

    feat: utilizando symbol

--- JavaScript-Objetos-e-prototypes-Avançado/aula81.js
+++ JavaScript-Objetos-e-prototypes-Avançado/aula81.js
@@ -1,1 +1,29 @@
 // Getters e Setters
+// para receber parametros as classes usam o construtor delas
+const _velocidade = Symbol('velocidade');
+class Carro {
+  constructor(nome) {
+    this.nome = nome;
+    this[_velocidade] = 0;
+  }
+
+  acelerar() {
+    if(this[_velocidade] >= 100) return;
+      this[_velocidade]+= 1;
+  }
+
+  freiar() {
+    if(this[_velocidade] <= 0) return
+    this[_velocidade]-=1;
+  }
+}
+
+const c1 = new Carro('Fusca');
+
+for(let i = 0; i <= 200; i+= 1) {
+  c1.acelerar();
+}
+
+c1.velocidade = 1500;
+
+console.log(c1)
