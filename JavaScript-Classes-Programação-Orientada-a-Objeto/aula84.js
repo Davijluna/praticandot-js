6f01ee05c07a3254651bada4d4a588ebd06de344
davi <davijluna@gmail.com>
2023-08-09 20:01:37 -0300

    feat: criando validações para o cpf usando class

--- JavaScript-Classes-Programação-Orientada-a-Objeto/aula84.js
+++ JavaScript-Classes-Programação-Orientada-a-Objeto/aula84.js
@@ -1,1 +1,24 @@
 // Usando classes (Validando CPF)
+const cpf = "358.484.248-45"
+
+class ValidaCPF {
+  constructor(cpfEnviando) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpfEnviando.replace(/\D+/g, '')
+    });
+  }
+
+
+  valida() {
+    if(!this.cpfLimpo) return false;
+    if(typeof this.cpflimpo !== 'string') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    return 'Cheguei Aqui'
+  }
+}
+
+const validacpf = new ValidaCPF(cpf)
+console.log(validacpf.valida());
