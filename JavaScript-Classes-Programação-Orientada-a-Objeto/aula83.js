5e3b3991c06caf8b717d1667a093391b063972dd
davi <davijluna@gmail.com>
2023-08-09 23:44:28 -0300

    feat: esátiso e instância

--- JavaScript-Classes-Programação-Orientada-a-Objeto/aula83.js
+++ JavaScript-Classes-Programação-Orientada-a-Objeto/aula83.js
@@ -1,1 +1,31 @@
 // Métodos de instância e estâticos.
+class ControleRemoto {
+  constructor(tv) {
+    this.tv = tv;
+    this.volume = 0
+  }
+
+   // Método de instância
+  aumentarVolume() {
+    this.volume += 2;
+  }
+
+  // Método de instância
+  diminuirVolume() {
+    this.volume -= 2;
+  }
+
+  // Método estático
+  trocaPilha() {
+    console.log('vou trocar!!!!')
+  }
+}
+
+const controle2 = new ControleRemoto('LG')
+const controle1 = new ControleRemoto('LG')
+
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+
+console.log(controle1, controle2)
