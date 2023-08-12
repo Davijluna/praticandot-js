ba8239f8ec0e9b5467e4bc79bb610ddfb3443f1c
davi <davijluna@gmail.com>
2023-08-11 21:25:16 -0300

    feat: adicionando arquivo para aula 84 e Rascunho de exercicio

--- RASCUNHO.js
+++ RASCUNHO.js
@@ -7,26 +7,35 @@ class Personagen {
     this.vida = vida;
   }
 
+  static chuva() {
+    console.log('Molhado')
+  }
+
   ataque() {
-    if(this.vida >= 2) {
+    if(this.vida > 2) {
      this.vida-= 2;
      console.log('vida :', typeof this.vida)
-    //  return this.vida
-    } if(this.vida < 2) {
-      console.log('Você Perdeu.');
+    } if(this.vida == 2) {
+      console.log('Corra você precisa correr');
+    }
+    if(this.vida < 2) {
+      console.log('você perdeu')
     }
   }
 }
 
-const Heroi = new Personagen('Olavo', 'Guerreiro', 2, 10)
+const Heroi = new Personagen('Olavo', 'Guerreiro', 2, 10, 'seco')
+const Campones = new Personagen('Piedro', 'Lavrador', 1, 1, 'seco')
 
 Heroi.ataque()
 Heroi.ataque()
 Heroi.ataque()
-Heroi.ataque()
-Heroi.ataque()
+
+Personagen.chuva()
+
 
 console.log(Heroi)
+console.log(Campones)
 
 // function pessoa(nome, idade) {
 //   return {
