97d10b1001d667274a735dd85ee10e8813fa0cb9
davi <davijluna@gmail.com>
2023-08-16 15:12:53 -0300

    feat: adicionando arquivo de validação de CPF e tratando os campos não preenchidos

--- JavaScript-Classes-Programação-Orientada-a-Objeto/aula85/script.js
+++ JavaScript-Classes-Programação-Orientada-a-Objeto/aula85/script.js
@@ -18,20 +18,40 @@ class ValidaFormulario {
   camposSaoValidos() {
     let valid = true;
 
+    for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+      errorText.remove();
+    }
+  
     for(let campo of this.formulario.querySelectorAll('.validar')) {
+      const label = campo.previousElementSibling.innerText;
       if(!campo.value) {
-        this.criaError(campo, "Campo tal não pode estar em branco")
+        this.criaError(campo, `Campo ${label} não pode estar em branco`)
         valid = false;
       }
 
+      if(campo.classList.contains('cpf')) {
+        if(!this.validaCPF(campo)) valid = false;
+      }
     }
   }
-  criaError(campo, msg) {
+    validaCPF(campo) {
+      const cpf = new ValidaCPF(campo.value);
+  
+      if(!cpf.valida()) {
+        this.criaError(campo, 'CPF inválido.');
+        return false;
+      }
+      
+      return true;
+    }
 
+  criaError(campo, msg) {
+    
     const div = document.createElement('div');
     div.innerHTML = msg;
     div.classList.add('error-text');
     campo.insertAdjacentElement('afterend', div);
+
   }
 }
 
---
+++ JavaScript-Classes-Programação-Orientada-a-Objeto/aula85/validaCPF.js
@@ -0,0 +1,55 @@
+// Usando classes (Validando CPF)
+// const cpf = "358.484.248-45"
+
+class ValidaCPF {
+  constructor(cpfEnviando) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpfEnviando.replace(/\D+/g, '')
+    });
+  }
+
+  éSequência() {
+    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+  }
+
+  geraNovoCPF() {
+    const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+    this.novoCPF = cpfSemDigitos + digito1 + digito2;
+  }
+// Quando o metodo não possue a palavra this ele pode ser static.
+  static geraDigito(cpfSemDigitos) {
+    let total = 0;
+    let reverso = cpfSemDigitos.length + 1;
+
+    for(let stringNumerica of cpfSemDigitos ) {
+      total += reverso * Number(stringNumerica);
+      console.log(typeof total);
+      reverso -= 1;
+    }
+
+    const digito = 11 - (total % 11);
+    return digito <= 9 ? String(digito) : '0';
+  }
+
+  valida() {
+    if(!this.cpfLimpo) return false;
+    if(typeof this.cpfLimpo !== 'string') return false;
+    if(this.éSequência()) return false;
+    this.geraNovoCPF();
+    console.log(this.novoCPF)
+
+    return this.novoCPF === this.cpfLimpo;
+  }
+}
+// ver para mais tarde.
+// const validacpf = new ValidaCPF('358.484.248-45');
+// console.log(validacpf.valida());
+
+// if(validacpf.valida()) {
+//   console.log('CPF VALIDO ');
+// } 
