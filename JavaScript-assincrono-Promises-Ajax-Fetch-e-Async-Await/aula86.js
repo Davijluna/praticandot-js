c7c57119b654784c79212b1e3663ab1e662777dd
davi <davijluna@gmail.com>
2023-08-23 17:22:53 -0300

    feat: metodo rando e setTimeut para simular promise

--- JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula86.js
+++ JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula86.js
@@ -1,1 +1,16 @@
 // Promises
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  setTimeout(() => {
+     console.log(msg);
+  }, tempo)
+}
+
+esperaAi('frase1', rand(1, 3))
+esperaAi('frase2', rand(1, 3))
+esperaAi('frase3', rand(1, 3))
