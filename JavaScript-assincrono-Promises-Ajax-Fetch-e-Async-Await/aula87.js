42b01dcb04f729e279f955b1d77edfba9b6d84af
davi <davijluna@gmail.com>
2023-08-25 12:05:40 -0300

    feat: trabalhando com Promise.all

--- JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula87.js
+++ JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula87.js
@@ -16,5 +16,20 @@ function esperaAi(msg, tempo) {
   });
 }
 
+const promise = [
+  'primeiro valor',
+  esperaAi('Promise 1', 3000),
+  esperaAi('Promise 2', 500),
+  esperaAi('Promise 3', 10000),
+  'outro valor'
+];
+
+Promise.all(promise)
+.then(function(valor) {
+  console.log(valor)
+}).catch(function(e) {
+  console.log(e, 'foi mostrado um erro')
+});
+
 // METODOS PASSADO EM AULA
 // Promise.all Promise.race Promise.resolve Promese.reject
