1e141592a80b68cc3d3e685784e23433cee2ac3a
davi <davijluna@gmail.com>
2023-08-25 16:17:51 -0300

    feat: modificações

--- JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula89/modelo/assets/js/aula89.js
+++ JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula89/modelo/assets/js/aula89.js
@@ -1,1 +1,20 @@
 // XMLHttpResquest (GET) + Promises
+// CÓDIGO HTTP.
+/**
+wikipédia
+https://pt.wikipedia.org/wiki/Lista_de_c%C3%B3digos_de_estado_HTTP
+
+MDN
+https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+ */
+
+const resquest = obj => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(obj.method, obj.url, true);
+  xhr.send();
+
+  xhr.addEventListener('load', (e) => {
+
+  })
+}
+
