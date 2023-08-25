c0fb90867c210e8de9188d11946a31b59b2adf1b
davi <davijluna@gmail.com>
2023-08-25 11:26:02 -0300

    feat: conhecendo alguns dos métodos

--- JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula87.js
+++ JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula87.js
@@ -1,1 +1,20 @@
 // Métodos úteis para Promises.
+
+
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    if(typeof msg !== 'string') reject(false);
+    setTimeout(() => {
+      resolve(msg);
+    }, tempo)
+  });
+}
+
+// METODOS PASSADO EM AULA
+// Promise.all Promise.race Promise.resolve Promese.reject
