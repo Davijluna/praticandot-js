1e263286d3f2f40b2a394ea86078c82b62adc329
davi <davijluna@gmail.com>
2023-08-25 17:31:31 -0300

    feat: colocando página em funcionamento com xml

--- JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula89/modelo/assets/js/aula89.js
+++ JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula89/modelo/assets/js/aula89.js
@@ -13,8 +13,45 @@ const resquest = obj => {
   xhr.open(obj.method, obj.url, true);
   xhr.send();
 
-  xhr.addEventListener('load', (e) => {
-
+  xhr.addEventListener('load', () => {
+    if(xhr.status >= 200 && xhr.status < 300) {
+      obj.sucess(xhr.responseText);
+    } else {
+      obj.error({
+        code: xhr.status,
+        msg: xhr.status
+      });
+    }
   })
+}
+
+document.addEventListener('click', e => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if(tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+})
+
+function carregaPagina(el) {
+  const href = el.getAttribute('href')
+  // console.log(href)
+  resquest({
+    method: 'GET',
+    url: href,
+    sucess(response) {
+      carregaResultado(response);
+    },
+    error(errorText) {
+      console.log(errorText)
+    }
+  })
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado')
+  resultado.innerHTML = response
 }
 
