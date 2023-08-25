a46448dfe38d36c3ebb48aa40565e6cabf93f347
davi <davijluna@gmail.com>
2023-08-25 15:07:47 -0300

    feat: adicionando função com  async e await e explicação com os três estados básicos das Promises

--- JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula88.js
+++ JavaScript-assincrono-Promises-Ajax-Fetch-e-Async-Await/aula88.js
@@ -44,4 +44,30 @@ function esperaAi(msg, tempo) {
 
 // console.log(rand())
 
-async function
+async function executar() {
+  try {
+
+    const fase1 = await esperaAi('Fase 1', rand())
+    console.log(fase1);
+  
+    const fase2 = await esperaAi('Fase 2', rand())
+    console.log(fase2);
+  
+    const fase3 = await esperaAi('Fase 3', rand())
+    console.log(fase3);
+  
+    console.log('Termina na fase: ', fase3);
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+// os três estados de uma Promise
+/**
+ * Pending (Pendente): Quando uma promise é criada, ela começa no estado "pendente". Isso significa que a operação associada à promise ainda não foi concluída.
+
+Fulfilled (Realizada): Uma promise passa para o estado "realizada" quando a operação associada a ela é bem-sucedida. Isso significa que o valor prometido está disponível, e a promise "entrega" esse valor para qualquer código que estiver aguardando.
+
+Rejected (Rejeitada): Uma promise entra no estado "rejeitada" quando a operação associada a ela falha de alguma forma. Isso pode ser devido a um erro lançado durante a execução da operação. Quando uma promise é rejeitada, ela fornece um motivo (geralmente um objeto de erro) que descreve a razão da rejeição.
+ */
+executar();
