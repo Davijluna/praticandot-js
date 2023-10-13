72706340760985384a2c20fbf035341721176a5e
davi <davijluna@gmail.com>
2023-10-13 14:49:46 -0300

    feat: fazendo instalação e configuração do webpack.config

--- JavaScript-Tooling-e-ES6-Modules-Modulo/aula93/webpack.config.js
+++ JavaScript-Tooling-e-ES6-Modules-Modulo/aula93/webpack.config.js
@@ -1,12 +1,23 @@
 const path = require('path') // CommonJS
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(_dirname, 'public', 'assets', 'js'),
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
     filename: 'bundle.js'
   },
-  module: {},
+  module: {
+    rules:[{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }]
+  },
   devtool: 'source-map'
 };
