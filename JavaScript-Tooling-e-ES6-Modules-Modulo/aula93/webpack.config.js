ed7a8f2a2e7c61801aba0bfd1c9bfa440d9810a6
davi <davijluna@gmail.com>
2023-10-13 11:11:36 -0300

    feat: adicionando parte da congiguração do mapeamento do webpack

--- JavaScript-Tooling-e-ES6-Modules-Modulo/aula93/webpack.config.js
+++ JavaScript-Tooling-e-ES6-Modules-Modulo/aula93/webpack.config.js
@@ -2,4 +2,11 @@ const path = require('path') // CommonJS
 
 module.exports = {
   mode: 'development',
-}
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(_dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {},
+  devtool: 'source-map'
+};
