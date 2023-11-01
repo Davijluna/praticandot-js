f29247aacb21ccca8932fdf76e27861d1856a01b
davi <davijluna@gmail.com>
2023-10-31 23:53:50 -0300

    feat: adicionando teste

--- rascunho2.js
+++ rascunho2.js
@@ -29,6 +29,6 @@
 const number = [1, 4, 7, 0, 9];
 const recebe = '';
 for (let i = 0; i < number.length; i +=1) {
-  recebe += number[i];
+  recebe += recebe.push(number[i]);
   console.log(recebe)
 }
