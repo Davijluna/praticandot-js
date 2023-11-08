b8f3b8c4815c44de5d2f9d0f1abca5d6a5bbcbac
davi <davijluna@gmail.com>
2023-11-08 00:21:22 -0300

    feat: exericio com every

--- lições-e-rascunhos/metodos/every.js
+++ lições-e-rascunhos/metodos/every.js
@@ -1,1 +1,9 @@
 // *Array.prototype.every()
+/**
+ * TODO: Exemplo simples do every.
+ */
+const isBelowThreshold = (currentValue) => currentValue < 40;
+
+ const array1 = [1, 30, 39, 29, 10, 13];
+
+ console.log(array1.every((isBelowThreshold)))
