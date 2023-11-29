610fa53ba72a7daa207db6f5201e4b0ba0137602
davi <davijluna@gmail.com>
2023-11-29 20:23:45 -0300

    feat: exercicio com every

--- lições-e-rascunhos/metodos/every.js
+++ lições-e-rascunhos/metodos/every.js
@@ -2,15 +2,22 @@
 /**
  * TODO: Exemplo simples do every.
  */
+
+const isBelowThreshold = (currentValue) => currentValue <= 40
+
+const array1 = [1, 30, 39, 29, 10, 13, 40];
+
+console.log(array1.every(isBelowThreshold))
+
 // const isBelowThreshold = (currentValue) => currentValue < 40;
 
 //  const array1 = [1, 30, 39, 29, 10, 13];
 
 //  console.log(array1.every((isBelowThreshold)))
 
-const isSubset = (array1, array2) => {
-  return array2.every((element) => array1.includes(element));
-}
+// const isSubset = (array1, array2) => {
+//   return array2.every((element) => array1.includes(element));
+// }
 
 
-console.log(isSubset([1, 3, 4, 5, 9, 7], [5, 9, 0]));
+// console.log(isSubset([1, 3, 4, 5, 9, 7], [5, 9, 0]));
