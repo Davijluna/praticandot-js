0a019cf6320c0fc9cc52f4a10d03dfeb80855a27
davi <davijluna@gmail.com>
2023-12-04 20:58:00 -0300

    feat: adicionando mais um exemplo

--- lições-e-rascunhos/metodos/forEach.js
+++ lições-e-rascunhos/metodos/forEach.js
@@ -1,14 +1,24 @@
 const data = [
-  { nome: "Davi", idade: 36 },
-  { nome: "João", idade: 32 },
-  { nome: "Maria", idade: 28 },
-  { nome: "Marta", idade: 21 }
+  { nome: "Davi", idade: 36, avaliacao: false },
+  { nome: "João", idade: 32, avaliacao: false },
+  { nome: "Maria", idade: 28, avaliacao: false },
+  { nome: "Marta", idade: 21, avaliacao: false }
 ]
 
 const showData = (users) => {
  users.forEach((user) => {
-  console.log(`Olá ${user.idade}`)
+  console.log(`Olá ${user.nome}`)
  }) 
 }
 
 showData(data)
+
+let someShowData = data.some((user) => user.avaliacao)
+
+console.log(someShowData);
+
+data[0].avaliacao = true
+console.log(data)
+
+someShowData = data.some((user) => user.avaliacao)
+console.log(someShowData)
