25c32396caa241acf7fa1702abb1c0d181c2adbd
davi <davijluna@gmail.com>
2023-12-04 21:19:18 -0300

    feat: adicionando exercicio com every

--- lições-e-rascunhos/metodos/every.js
+++ lições-e-rascunhos/metodos/every.js
@@ -3,12 +3,23 @@
  * TODO: Exemplo simples do every.
  */
 
-const isBelowThreshold = (currentValue) => currentValue <= 40
+// const isBelowThreshold = (currentValue) => currentValue <= 40
 
-const array1 = [1, 30, 39, 29, 10, 13, 40];
+// const array1 = [1, 30, 39, 29, 10, 13, 40];
 
-console.log(array1.every(isBelowThreshold))
-// reinicio de aula
+// console.log(array1.every(isBelowThreshold))
+
+
+const data = [
+  { nome: "Davi", idade: 36, avaliacao: false, salario: 13000 },
+  { nome: "João", idade: 32, avaliacao: false, salario: 3000},
+  { nome: "Maria", idade: 28, avaliacao: false, salario: 2500 },
+  { nome: "Marta", idade: 21, avaliacao: false, salario: 4500 }
+]
+
+ const showData = data.every((valor) => valor.salario > 1000);
+
+ console.log(showData)
 
 // const isBelowThreshold = (currentValue) => currentValue < 40;
 
