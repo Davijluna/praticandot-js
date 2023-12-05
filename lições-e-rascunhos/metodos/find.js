51cd7180c0539d63e159e9f2f93d2b5d2c8657e5
davi <davijluna@gmail.com>
2023-12-04 21:29:24 -0300

    feat: primeiro teste da utilização do find para encontrar primeiro valor

---
+++ lições-e-rascunhos/metodos/find.js
@@ -0,0 +1,9 @@
+// Trabalhando com o metodo find
+
+
+// Utilizando find para encontrar o primeiro valor.
+const array = [50, 100, 330, 104, 9]
+
+const funcArray = array.find((number) => number < 40)
+
+console.log(funcArray)
