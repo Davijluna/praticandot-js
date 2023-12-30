5daecf519e3b80463f276c10803b45e3a3ff2466
davi <davijluna@gmail.com>
2023-12-30 17:01:37 -0300

    feat: finalizando gerador de CPFs

--- JavaScript-Tooling-e-ES6-Modules-Modulo/aula95/src/modules/GeraCPF.js
+++ JavaScript-Tooling-e-ES6-Modules-Modulo/aula95/src/modules/GeraCPF.js
@@ -5,11 +5,21 @@ export default class GeraCPF {
     return String(Math.floor(Math.random() * (max - min) + min));
   }
 
+  formatado(cpf) {
+    return (
+      cpf.slice(0, 3) + '.' +
+      cpf.slice(3, 6) + '.' +
+      cpf.slice(6, 9) + '-' +
+      cpf.slice(9, 11)
+
+    );
+  }
+
   geraNovoCpf() {
     const cpfSemDigito = this.rand();
     const digito1 = ValidaCPF.geraDigito(cpfSemDigito);
     const digito2 = ValidaCPF.geraDigito(cpfSemDigito + digito1);
     const novoCpf = cpfSemDigito + digito1 + digito2;
-    return novoCpf;
+    return this.formatado(novoCpf);
   }
 }
