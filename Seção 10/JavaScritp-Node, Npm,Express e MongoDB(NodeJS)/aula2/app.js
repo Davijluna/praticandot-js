9efe28961a96b5aedf9c06b2190f9f28f7e70155
davi <davijluna@gmail.com>
2024-02-19 20:10:11 -0300

    feat: seção 10 aula 2

---
+++ Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula2/app.js
@@ -0,0 +1,15 @@
+const Cachorro = require('./Z/mod2');
+
+const c1 = new Cachorro('Dog')
+c1.latir()
+
+// const path = require('path')
+
+
+// console.log(path.resolve(__dirname, '..', '..'));
+// console.log(__filename);
+// console.log(__dirname);
+
+// console.log(Cachorro)
+// const cachorrinho = new Cachorro('Dog');
+// cachorrinho.latir();
