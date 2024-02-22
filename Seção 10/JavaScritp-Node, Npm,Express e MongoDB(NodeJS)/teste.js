646e9a473ffb623142352a63b4126a04c97cd2de
davi <davijluna@gmail.com>
2024-02-21 22:15:57 -0300

    desafio rapido do hackerRank

--- Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/teste.js
+++ Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/teste.js
@@ -1,3 +1,23 @@
-const nome = "23:23";
+function timeConversion(s) {
+  let strinSlice = s.slice(0, 8);
+  const hoursSplit = strinSlice.replace(/[^0-9]/g, '');
+  let number0 = hoursSplit[0];
+  let number1 = hoursSplit[1];
+  let joinNumber = number0.concat(number1);
+  let numberPM = Number(joinNumber) + 12
+  let concatNumber = s.slice(2, 8)
+  
+  if(s.slice(-2) === "PM" && joinNumber < 12) {
+      console.log(numberPM + concatNumber)
+  } if(s.slice(-2) === "AM" && joinNumber == 12) {
+      console.log("00" + concatNumber)
+  }else {
+      console.log(hoursSplit[0]+hoursSplit[1]+concatNumber)
+  }
 
-console.log(nome.replace(/[^0-9]/g, ''))
+}
+
+const s = "06:40:03AM";
+// valor esperado > 06:40:03
+
+timeConversion(s);
