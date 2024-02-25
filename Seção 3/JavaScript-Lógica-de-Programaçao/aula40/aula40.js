be468a05b1c171a1c743dbd36b240257dd4d5749
davi <davijluna@gmail.com>
2024-02-25 01:19:02 -0300

    feat: seção 3 utilizando continue e break

--- Seção 3/JavaScript-Lógica-de-Programaçao/aula40/aula40.js
+++ Seção 3/JavaScript-Lógica-de-Programaçao/aula40/aula40.js
@@ -1,12 +1,19 @@
 // ** Utilizando "continue" para pular o laço for.
+// ** Utilizando "Break"
 
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for(let numero of numeros) {
 
   if(numero === 2) {
+    console.log('Pulei o número 2')
     continue;
   }
 
   console.log(numero)
+  if(numero === 7) {
+    console.log('7 encontrado, saindo...')
+    break;
+  }
+
 }
