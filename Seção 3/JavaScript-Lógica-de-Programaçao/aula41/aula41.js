d129274b38313493d830ed1d2bab691b2fc8fe15
davi <davijluna@gmail.com>
2024-02-25 01:00:50 -0300

    feat: seção 3 exercicio de lógica de pragramação

---
+++ Seção 3/JavaScript-Lógica-de-Programaçao/aula41/aula41.js
@@ -0,0 +1,4 @@
+// ** Escreva uma função que recece 2 números e retorne o maior deles.
+
+const maxNumber = (x, y) => x > y ? `${x} que é x é maior` : `${y} que é y é maior`;
+console.log(maxNumber(2000, 10000))
