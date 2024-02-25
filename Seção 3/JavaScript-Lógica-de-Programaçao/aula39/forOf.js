29f98d29424249f0146642f6e8d1d7d75b523fec
davi <davijluna@gmail.com>
2024-02-25 01:20:26 -0300

    seção 3 colocando forOf para teste de lógica

--- Seção 3/JavaScript-Lógica-de-Programaçao/aula39/forOf.js
+++ Seção 3/JavaScript-Lógica-de-Programaçao/aula39/forOf.js
@@ -1,10 +1,27 @@
-const nome = "Davi Jesus de Luna";
+const array = ["Mario", "Maria", "José", "Algusto", "Natalia"];
+let notUser = 0
+let getUser = 0
 
-for ( string of nome) {
-  if(string === 'a') {
-    console.log('temos uma letra A')
-  } else{
-    console.log(string)
-  }
-
+function teste(x, y) {
+  return x + y;
 }
+
+// for(nome of array) {
+//   if(nome === "Maria") getUser += 1
+//   else {
+//     notUser += 1
+//   }
+
+// }
+// console.log(`Maria apareceu ${getUser} vezes mas não apareceu ${notUser}`)
+
+console.log(teste(2, 3))
+
+// for ( string of nome) {
+//   if(string === 'a') {
+//     console.log('temos uma letra A')
+//   } else{
+//     console.log(string)
+//   }
+
+// }
