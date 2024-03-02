1560c93774aa516e0f50b1b11400064e9c5521c5
davi <davijluna@gmail.com>
2024-03-01 23:30:59 -0300

    feat: usando as funções test e exec nas expressões regulares

--- Seção 17/aquivo-de-exerciciosMDN/script.js
+++ Seção 17/aquivo-de-exerciciosMDN/script.js
@@ -6,11 +6,12 @@ const literal = /ab+c/;
 
 let re = new RegExp("ab+c");
 
-const texto = 'Na escola ensina o'
+const texto = 'Na escola ensina o abc'
 
 
+const result = literal.exec(texto)[0]
 if(literal.test(texto)) {
-  console.log('sim')
+  console.log( result)
 } else{
-  console.log('não')
+  console.log('não', result)
 }
