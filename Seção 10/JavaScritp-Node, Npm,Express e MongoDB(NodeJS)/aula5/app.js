9e64f04cc8a340ac6ba22ada867f35d56784eece
davi <davijluna@gmail.com>
2024-03-03 21:29:06 -0300

    feat: seção 10 aula 5 lendo arquivos

--- Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula5/app.js
+++ Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula5/app.js
@@ -1,16 +1,36 @@
 const path = require('path');
 const caminhoArquivo = path.resolve(__dirname, 'teste.json')
 const escreve = require('./modules/escrever')
+const ler = require('./modules/ler');
 
+// const pessoas = [
+//   {nome: 'João'},
+//   {nome: 'Davi'},
+//   {nome: 'Maria'},
+//   {nome: 'Mara'},
+// ];
 
-const pessoas = [
-  {nome: 'João'},
-  {nome: 'Davi'},
-  {nome: 'Maria'},
-  {nome: 'Mara'},
-];
+// // Todo: covertendo para json:
 
-// Todo: covertendo para json:
+// const json = JSON.stringify(pessoas, '', 2);
+// escreve(caminhoArquivo, json);
 
-const json = JSON.stringify(pessoas, '', 2);
-escreve(caminhoArquivo, json);
+async function leArquivo(caminho) {
+ const dados = await ler(caminho);
+ renderizaDados(dados);
+//  return dados;
+}
+
+function renderizaDados(dados) {
+  // console.log(dados)
+  // TODO: desconvertendo de JSON para Objeto 
+   dados = JSON.parse(dados);
+   dados.forEach(valor => {
+    console.log(valor.nome)
+   });
+};
+
+leArquivo(caminhoArquivo)
+
+// const dadosArquivo = leArquivo(caminhoArquivo).then(dados => console.log(dados));
+// // console.log(dadosArquivo)
