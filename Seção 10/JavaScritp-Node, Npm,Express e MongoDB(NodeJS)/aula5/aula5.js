0ed66e61c9bafa7a7e0a6a6a64be5cb34fada9de
davi <davijluna@gmail.com>
2024-03-04 22:57:31 -0300

    feat: adição de logs para funções parse e stringify

--- Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula5/aula5.js
+++ Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula5/aula5.js
@@ -1,2 +1,19 @@
 // ** Escrevendo e lendo arquivo
 
+const objeto = [
+  {
+    nome: 'Davi',
+    sobrenome: 'Jesus de Luna',
+    idade: 36,
+    sexo: 'M'
+  }
+]
+
+
+const json = JSON.stringify(objeto)
+const dados = JSON.parse(json)
+
+console.log(typeof objeto); // ** continua como objeto
+
+console.log(dados, 'convertido'); // ** convertemos o JSON para objeto novamente.
+console.log(typeof json); // ** o objeto está em JSON
