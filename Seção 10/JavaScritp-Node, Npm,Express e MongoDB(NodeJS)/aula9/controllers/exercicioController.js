1598d007f350f5e1b82eabd6e5c8feabf7254899
davi <davijluna@gmail.com>
2024-03-16 13:33:02 -0300

    feat: rota de exercicio

---
+++ Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula9/controllers/exercicioController.js
@@ -0,0 +1,3 @@
+exports.exercicio = (req, res) => {
+  res.send('exercicio. ')
+}
--- Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula9/routes.js
+++ Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula9/routes.js
@@ -3,6 +3,8 @@ const route = express.Router() // ? Utilizando router com express
 const homeController = require('./controllers/homeControllers') // ** Aqui eu importei o homeController do caminho mostrado.
 const contatoController = require('./controllers/contatoController')
 
+const exercicioController = require('./controllers/exercicioController')
+
 // Rotas da Home
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost);
@@ -12,6 +14,10 @@ route.get('/contato', contatoController.paginaInicial)
 
 // route.get('/', (req, res) => {
 // })
+
+// Todo: minha rota de exercicio.
+
+route.post('/exercicio', exercicioController.exercicio)
 
 
 
