175e5e378e664b5e21ea5f43a34289f1bac28fb1
davi <davijluna@gmail.com>
2024-04-10 22:06:27 -0300

    corrigindo nome

---
+++ Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula15B/aula15/src/middleware/middleware.js
@@ -0,0 +1,9 @@
+exports.middlewareGlobal = (req, res, next) => {
+  // console.log('Primeiro middleware')
+  next();
+};
+
+exports.outroMiddleware = (req, res, next) => {
+  // console.log('Seu outro middleware');
+  next()
+}
