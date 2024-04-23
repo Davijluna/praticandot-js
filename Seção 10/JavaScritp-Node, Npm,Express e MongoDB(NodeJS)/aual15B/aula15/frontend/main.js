f4e2d34480692770c4c94efd088fcadb54738053
davi <davijluna@gmail.com>
2024-04-23 18:59:08 -0300

    alterações para o gitignore e demais arquivos

--- Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aual15B/aula15/frontend/main.js
+++
@@ -1,6 +0,0 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime'
-
-import './assets/css/style.css';
-
-alert(1)
--- Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aual15B/aula15/src/controllers/contatoController.js
+++
@@ -1,5 +0,0 @@
-// TODO: Aqui temos um middleware.
-
-exports.paginaInicial = (req, res) => {
-  res.send('Obrigado por entrar em contato.')
-}
