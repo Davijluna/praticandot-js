7f4ce076dfc3eaca8920ee109d7a0904443beefe
davi <davijluna@gmail.com>
2024-05-11 12:05:28 -0300

    feat: alterações

--- Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula16/src/controllers/homeControllers.js
+++ Seção 10/JavaScritp-Node, Npm,Express e MongoDB(NodeJS)/aula16/src/controllers/homeControllers.js
@@ -8,7 +8,10 @@
   // .catch(e => console.log(e));
 
 exports.paginaInicial = (req, res) => {
-    res.render('index');
+    res.render('index', {
+      titulo:'este será o titulo',
+      numero:[0,1,2,3,4,5,6,7]
+    });
  return;
 }
 
